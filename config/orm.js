f69373a4261ef6f40b404614cc0ba119a9947511
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-12-11 10:02:46 -0800

    yuh

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,31 @@
-var connection = require("/.connection");
+var connection = require("./connection.js");
+
+var orm = {
+    // The last variable cb represents the anonymous function being passed from server.js
+    findAll: function (tableInput, colToSearch, valOfCol, cb) {
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            cb(result);
+        });
+
+    },
+
+    insertOne: function (tableInput, colToSearch, valOfCol, cb) {
+        var queryString = "INSERT into burgers(";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            cb(result);
+        });
+
+    },
+
+    updateOne: function (tableInput, colToSearch, valOfCol, cb) {
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            cb(result);
+        });
+
+    }
+
+};
+
+module.exports = orm;
